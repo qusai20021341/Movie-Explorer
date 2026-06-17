ad62333bd3a340ba19b37c5b4c19a2d6ad3f4b5c
Qusai AlZmaily <qusaialzmaily3@gmail.com>
2026-06-18 01:24:21 +0300

    latest screen update

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,11 +1,8 @@
 import axios from "axios";
 import MovieCard from "../components/MovieCard";
 import { useEffect, useState } from "react";
-const Favorites = ({
-  searchText,
-  favoriteMoviesList,
-  setFavoriteMoviesList,
-}) => {
+
+const Favorites = ({ searchText, favoriteMoviesList, setFavoriteMoviesList }) => {
   const [movies, setMoives] = useState([]);
   const [load, setLoad] = useState(false);
 
@@ -34,28 +31,34 @@ const Favorites = ({
       setMoives([]);
     }
   }, [favoriteMoviesList]);
+
   const filteredMovies = movies.filter((movie) =>
     movie.title.toLowerCase().includes(searchText.trim().toLowerCase()),
   );
 
   return (
-    <div className="bg-dark px-5" style={{ minHeight: "100vh" }}>
-      <h5 className="display-5 py-4 text-white">Favorites:</h5>
-
-      {favoriteMoviesList.length == 0 ? (
-        <h3 className="text-white text-center display-6 pt-5">You'll find your favorite movies here!</h3>
-      ) : load ? (
-        <p className="text-white text-center">Loading...</p>
+    <div className="bg-dark" style={{ minHeight: "100vh", position: "relative" }}>
+      {load ? (
+        <p>Loading...</p>
       ) : (
-        <div className="movies d-flex flex-wrap gap-3 pb-2 justify-content-center">
-          {(searchText.length === 0 ? movies : filteredMovies).map((movie) => (
-            <MovieCard
-              key={movie.id}
-              movie={movie}
-              favoriteMoviesList={favoriteMoviesList}
-              setFavoriteMoviesList={setFavoriteMoviesList}
-            />
-          ))}
+        <div>
+          <h5 className="display-5 py-4 px-5 text-white">Favorites:</h5>
+          {favoriteMoviesList.length == 0 ? (
+            <h3 className="text-white text-center display-6 pt-5">
+              You'll find your favorite movies here!
+            </h3>
+          ) : (
+            <div className="movies d-flex flex-wrap gap-3 pb-2 justify-content-center ">
+              {(searchText.length === 0 ? movies : filteredMovies).map((movie) => (
+                <MovieCard
+                  key={movie.id}
+                  movie={movie}
+                  favoriteMoviesList={favoriteMoviesList}
+                  setFavoriteMoviesList={setFavoriteMoviesList}
+                />
+              ))}
+            </div>
+          )}
         </div>
       )}
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
-const Home = ({ searchText,favoriteMoviesList,setFavoriteMoviesList }) => {
+
+const Home = ({ searchText, favoriteMoviesList, setFavoriteMoviesList }) => {
   const [trendingMoviesList, setTrendingMoviesList] = useState([]);
-  const [load, setLoad]=useState(false)
+  const [load, setLoad] = useState(false);
 
   useEffect(() => {
     const getTrendingMoives = async () => {
-    setLoad(true)
+      setLoad(true);
       const result = await axios.get(
         "https://api.themoviedb.org/3/trending/movie/week?api_key=165bfd050a291be69f9ccec37b38792d",
       );
@@ -16,27 +17,39 @@ const Home = ({ searchText,favoriteMoviesList,setFavoriteMoviesList }) => {
     };
     getTrendingMoives();
   }, []);
+
   const filteredMovies = trendingMoviesList.filter((movie) =>
     movie.title.toLowerCase().includes(searchText.toLowerCase()),
   );
 
   return (
-    
-
-    <div className="px-5 bg-dark" style={{ minHeight: "100vh" }}>
-      <h5 className="display-5 py-4 text-white"> Trending:</h5>
-      {
-        load?<h3 className="text-white">Loading...</h3>:
-<div className="movies d-flex flex-wrap gap-3 pb-2 justify-content-center">
+    <div className="bg-dark" style={{ minHeight: "100vh", position: "relative" }}>
+      {load ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <h5 className="display-5 py-4 px-5 text-white">Trending:</h5>
+          <div className="movies d-flex flex-wrap gap-3 pb-2 justify-content-center ">
             {searchText.trim().length == 0
-            ? trendingMoviesList.map((movie) => (
-                <MovieCard key={movie.id} movie={movie} favoriteMoviesList={favoriteMoviesList} setFavoriteMoviesList={setFavoriteMoviesList} />
+              ? trendingMoviesList.map((movie) => (
+                  <MovieCard
+                    key={movie.id}
+                    movie={movie}
+                    favoriteMoviesList={favoriteMoviesList}
+                    setFavoriteMoviesList={setFavoriteMoviesList}
+                  />
                 ))
-            : filteredMovies.map((movie) => (
-                <MovieCard key={movie.id} movie={movie} favoriteMoviesList={favoriteMoviesList} setFavoriteMoviesList={setFavoriteMoviesList} />
+              : filteredMovies.map((movie) => (
+                  <MovieCard
+                    key={movie.id}
+                    movie={movie}
+                    favoriteMoviesList={favoriteMoviesList}
+                    setFavoriteMoviesList={setFavoriteMoviesList}
+                  />
                 ))}
+          </div>
         </div>
-       }   
+      )}
     </div>
   );
 };
